bc687b04f82876c51320cc02c87ba304034f121d
gharseldin <gharseldin@aucegypt.edu>
2021-03-06 15:40:30 -0800

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Amr Gharseldin on 3/6/21.
+//  Copyright © 2021 Gharseldin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
